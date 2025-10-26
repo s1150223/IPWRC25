d0d505d2d4dddca208fe66ce9aa18ba5f6bd6a2a
Bas Valk <s1150223@student.hsleiden.nl>
2025-10-26 22:54:54 +0100

    correct frontend api call

--- IPWRC-Front/src/app/services/order.service.ts
+++ IPWRC-Front/src/app/services/order.service.ts
@@ -7,8 +7,7 @@ import { Order, OrderItem } from '../models/order.model';
     providedIn: 'root'
 })
 export class OrderService {
-    private apiUrl = 'http://localhost:8080/api/orders';
-    // private apiUrl = 'https://ipwrc25back.onrender.com/api/orders';
+    private apiUrl = 'https://ipwrc25back2.onrender.com/api/orders';
 
     constructor(private http: HttpClient) {}
 
