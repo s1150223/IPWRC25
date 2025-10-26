4a54cb2c93d98b585efaf9a0d5acdc55c21145f3
Bas Valk <s1150223@student.hsleiden.nl>
2025-10-26 15:53:50 +0100

    changed cors to allow the new frontend

--- IPWRC-Back/src/main/java/org/basvalk/ipwrcback/Config/CorsConfig.java
+++ IPWRC-Back/src/main/java/org/basvalk/ipwrcback/Config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://ipwrc25.onrender.com", "http://localhost:4200", "https://ipwrc25back-dswp.onrender.com", "https://ipwrc25back-dswp.onrender.com/")
+                        .allowedOrigins("https://ipwrc25.onrender.com", "http://localhost:4200", "https://ipwrc25-hdfc.onrender.com/", "https://ipwrc25-hdfc.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
@@ -33,7 +33,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("https://ipwrc25.onrender.com/", "http://localhost:4200", "https://ipwrc25back-dswp.onrender.com", "https://ipwrc25back-dswp.onrender.com/"));
+        config.setAllowedOrigins(List.of("https://ipwrc25.onrender.com/", "http://localhost:4200", "https://ipwrc25-hdfc.onrender.com/", "https://ipwrc25-hdfc.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
