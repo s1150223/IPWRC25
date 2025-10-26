d67bcf011483f4014bcdec6dbbd73d2db4dbb2bf
Bas Valk <s1150223@student.hsleiden.nl>
2025-10-26 16:03:28 +0100

    Removed a /.

--- IPWRC-Back/src/main/java/org/basvalk/ipwrcback/Security/SecurityConfig.java
+++ IPWRC-Back/src/main/java/org/basvalk/ipwrcback/Security/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "https://ipwrc25.onrender.com/", "https://ipwrc25-hdfc.onrender.com/", "https://ipwrc25-hdfc.onrender.com"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "https://ipwrc25-hdfc.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
